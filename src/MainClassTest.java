0b79d872e94a13160010d4519617bf0b05ea3a01
fariza zhiyenbayeva <fariza.zhn@yandex.ru>
2020-06-17 11:49:34 +0300

    2nd task: check if the testGetClassNumber() returns value > 45

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -7,4 +7,9 @@ public class MainClassTest {
         Assert.assertTrue("The local number is " + MainClass.getLocalNumber() +
                 ". But must be 14", MainClass.getLocalNumber() == 14);
     }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("Class number must be greater than 45.", MainClass.getClassNumber() > 45);
+    }
 }
